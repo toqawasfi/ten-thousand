2c5ea53bbf3768759ff20333965312a40c9cc9ba
toqawasfi <toqawasfi123@gmail.com>
2023-05-01 14:07:44 +0000

    999' q git add .

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,15 +1,26 @@
 from game_logic import GameLogic
 # ten_thousand.
-from collections import Counter
 
 def play(roller=GameLogic.roll_dice, num_round=15):
+    '''
+    Play is a Method responsible for Running the game 
+    This method receives two parameters :
+     roller param: to generate random numbers for dices
+     num_round param: number rounds to roll the dice and the max allowed to user is 15 round
+     returns: Game version2 as its required.
 
+    '''
+  
     score = 0
     global dice_roller
     dice_roller = roller
     total =0
 
     def start_game(num_round):
+        '''
+       start_game is a Method responsible for starting the game ,exactly the first round and to represnt as sim file.
+       receives num_round param: to initiate the game round by round
+        '''
         nonlocal score
         dec_round = 0
         nonlocal total
@@ -28,23 +39,23 @@ def play(roller=GameLogic.roll_dice, num_round=15):
            
 
         def quit():
-            nonlocal values
+            '''
+             quit is a Method responsible for quitting the game ,when the user inser "q"
+       
+            '''
             print("Thanks for playing. You earned " + str(total) + " points")
 
         while num_round > 0:
-          
 
             dec_round += 1
             num_dice = 6
-           
+            
             print("Starting round " + str(dec_round))
-            values = dice_roller(6)
-            formatted_roll = format_roller(values)
+            formatted_roll = format_roller(dice_roller(6))
             print("Rolling 6 dice...")
             print(formatted_roll)
             print("Enter dice to keep, or (q)uit:")
-            
-           
+
             input2 = input("> ")
             rem_input2 = input2.replace("> ", "").strip()
             if rem_input2 == "q":
@@ -52,37 +63,16 @@ def play(roller=GameLogic.roll_dice, num_round=15):
                 return 
             else:
                 # bank_count_int=int(rem_input2)
-                
-                # if len(rem_input2)==1:
-                    bank_count_int=int(rem_input2)
-                    dices_rem = dice_roller(num_dice)
+                if len(rem_input2)==1:
+                    # bank_count_int=int(rem_input2)
                     score = GameLogic.calculate_score((int(rem_input2),))
                     num_dice -= 1
-                # if(tuple(rem_input2) not in values  ):
-                #       print("Cheater!!")
-                    
-                # else:
+                else:
                     list_num = [int(d) for d in rem_input2]
                     t= tuple(list_num)
                     score = GameLogic.calculate_score(t)
                     num_dice -= len(rem_input2)
-                    dices_rem = dice_roller(num_dice)
-                    
-                    for roll in t:
-                        if roll  not in values:
-                             print("Cheater!!")
-                    user_counter = Counter(t)
-                    # values_counter = Counter(values)
-                    rem_counter = Counter(dices_rem)
-                    # if (user_counter[1]>=rem_counter[1]):
-                    #      print("Cheater!!")
-
-                    for  i in range(1,7):
-                        if (user_counter[i]>rem_counter[i] and user_counter[i]!=0):
-                           print("Cheater!!")
-
-
-
+               
                 # score = GameLogic.calculate_score(rem_input2)
                 banked_count = score
                 print("You have " + str(banked_count) + " unbanked points and " + str(num_dice) + " dice remaining")
@@ -95,18 +85,18 @@ def play(roller=GameLogic.roll_dice, num_round=15):
                     # num_dice=
                 elif rem_input3 == "q":
                     quit()
-                # elif rem_input3 == "r":
-                #     dices_rem = dice_roller(num_dice)
-            
+                else:
+                    pass
+
             num_round -= 1
             num_dice = 0
-            # num_dice -= 1
-
            
-    # def cheat():
-       
+           
 
     def user_input():
+        '''
+         user_input is a Method responsible for acquiring inputs from user about what to bank and how many times to roll dices.
+        '''
         print("Welcome to Ten Thousand")
         print("(y)es to play or (n)o to decline")
         input1 = input("> ")
@@ -118,7 +108,6 @@ def play(roller=GameLogic.roll_dice, num_round=15):
         start_game(num_round)
     else:
         print("OK. Maybe another time")
-    
 
 def format_roller(dice_roller):
     as_string = [str(value) for value in dice_roller]   
